8155fccbf5e1268a4cd76e038e03582d25c6020b
fr4nkln11 <ikehfranklind3c0d3r@gmail.com>
2023-07-13 15:03:25 -0700

    4-puts.cbetty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that use puts to display text'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
