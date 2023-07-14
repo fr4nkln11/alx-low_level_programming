ea1a6f887c27781643cbaf779962431e8161c3f3
fr4nkln11 <ikehfranklind3c0d3r@gmail.com>
2023-07-14 14:38:02 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "solution for
+ * I sometimes suffer from insomnia.
+ * And when I can't fall asleep,
+ * I play what I call the alphabet game"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = '0'; alphabet <= '9'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
