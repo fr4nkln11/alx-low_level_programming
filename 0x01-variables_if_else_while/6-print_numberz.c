a5ff90217e2f5bb9d824b643c159c69e9b0be9c7
fr4nkln11 <ikehfranklind3c0d3r@gmail.com>
2023-07-14 14:30:48 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "solution for
+ * I sometimes suffer from insomnia.
+ * And when I can't fall asleep,
+ * I play what I call the alphabet game"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ascii;
+
+	for (ascii = 48; ascii <= 57; ascii++)
+	{
+		putchar(ascii);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
