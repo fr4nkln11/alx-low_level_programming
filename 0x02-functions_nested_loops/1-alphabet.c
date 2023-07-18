4bae19d7b57954da557e036101dff9e51000008a
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-18 21:45:09 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+        char alphabet;
+
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+                _putchar(alphabet);
+        }
+
+        _putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: "I sometimes suffer from insomnia.
+ * And when I can't fall asleep,
+ * I play what I call the alphabet game"
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
