4a9a149dc0158117507134bf395749d7040126ba
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-18 21:52:21 +0100

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
+/**
+ * print_alphabet - writes alphabet to screen
+ *
+ * Description: "Prints all
+ * the alphabets to the screen"
+ *
+ * Return: Returns void
+ */
 void print_alphabet(void)
 {
-        char alphabet;
+	char alphabet;
 
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-                _putchar(alphabet);
-        }
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
 
-        _putchar('\n');
+	_putchar('\n');
 }
