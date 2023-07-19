c0feff469d78b36afdd71cf7dd72a4b732bd74c9
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 22:08:17 +0100

    100-times_table.c4

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,11 +24,13 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar(product + '0');
 			}
 			else if (product < 100)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
@@ -36,6 +38,7 @@ void print_times_table(int n)
 			else
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar((product / 10) + '0');
 				_putchar(((product / 10) % 10) + '0');
 				_putchar((product % 10) + '0');
