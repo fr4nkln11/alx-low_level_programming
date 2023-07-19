a97ed725297e0bdca444534dee14d35035b6dcf3
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 21:47:21 +0100

    9-times_table.c3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,11 +18,7 @@ void times_table(void)
 		{
 			int product = i * j;
 
-			if (product == 0)
-			{
-				_putchar('0');
-			}
-			else if (product < 10)
+			if (product < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
