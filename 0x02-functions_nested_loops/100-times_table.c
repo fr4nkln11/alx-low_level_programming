9fedbf2f9a4c1d794583b3f6cdf9a8c81f61a7f1
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 22:04:03 +0100

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints time table
+ * @n: length of time table
+ *
+ * Return: void
+ */
+
+int print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('0');
+
+		for (j = 1; j < n; j++)
+		{
+			int product = i * j;
+
+			if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar((product / 10) + '0');
+				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+
+}
+
