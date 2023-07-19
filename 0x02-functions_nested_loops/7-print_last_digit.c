8659d63b12770538fe228b96efd76f4c9a2d87cf
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 20:16:10 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of an integer
+ * @n: integer value to check
+ *
+ * Return: last digit of integer
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
