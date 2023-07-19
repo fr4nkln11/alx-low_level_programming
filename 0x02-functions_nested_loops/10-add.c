56c3d3234f354ba956d48ba693a2a5e0d6487a58
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 21:50:21 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: sum of the two integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
