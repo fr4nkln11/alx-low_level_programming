f63fcdfe1a3c6f1ea9c55385aaa327960e3fe139
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 19:41:26 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an integer
+ * @n: integer to check
+ *
+ * Return: 1 for positive,
+ * 0 for zero,
+ * -1 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
