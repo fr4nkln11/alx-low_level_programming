695ef40809b4386e0876a2827b3d2f4dc5a5fd56
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-19 21:08:09 +0100

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of Jack Bauer's day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 2 && j > 3)
+			{
+				break;
+			}
+			else
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+					}
+				}
+			}
+		}
+	}
+}
