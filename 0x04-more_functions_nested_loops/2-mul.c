3c38377d9db1e5c19dfed090f1c75a09bf161b0c
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-20 22:09:19 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ *
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return: product of the two digits
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
