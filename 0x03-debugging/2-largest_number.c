f805226c4202456ce3361f99e4db6fcc19b67a5c
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-07-21 20:41:58 +0100

    2-largest_number.c2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	if (a > b && a > c)
+	int largest;
+
+	if (a >= b && a >= c)
 	{
-		return (a);
+		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
-		return (b);
+		largest = b;
 	}
-	else
+	else if (c >= a && c >= b)
 	{
-		return (c);
+		largest = c;
 	}
+
+	return (largest);
 }
