535824f76c0867e60f77acb0fd3fe4e3f85b1648
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-07 20:07:01 +0100

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
+	char *cat = dest;
+	int len = 0;
+
 	while (*dest != '\0')
 	{
+		*(cat + len) = *dest;
+		len++;
+		dest++;
 	}
+	while (*src != '\0')
+	{
+		*(cat + len) = *src;
+		len++;
+		src++;
+	}
+	*(cat + len) = '\0';
+
+	return (cat);
 }
