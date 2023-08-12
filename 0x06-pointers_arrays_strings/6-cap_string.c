32d4e4545aa369aefb7bc244645c1900e660fb64
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-12 21:47:53 +0100

    6-cap_string.c2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,12 +13,16 @@ char *cap_string(char *s)
 
 	index = 0;
 
+	/* while current character is not a null character ('\0') */
 	while (*(s + index) != '\0')
 	{
 		char prev_char = *(s + (index - 1));
 		char current_char = *(s + index);
 
-		/* check if previous character is a non letter*/
+		/**
+		 * check if previous character is a non letter,
+		 * a non digit and not '-'
+		 */
 		if (
 		!((prev_char > 64 && prev_char < 91)
 			|| (prev_char > 96 && prev_char < 123))
@@ -26,12 +30,15 @@ char *cap_string(char *s)
 		&& (prev_char != 45)
 		)
 		{
+			/* check if letter is lower case*/
 			if (current_char > 96 && current_char < 123)
 			{
+				/* convert letter to upper case */
 				*(s + index) -= 32;
 			}
 		}
 
+		/* increment array index */
 		index++;
 	}
 
