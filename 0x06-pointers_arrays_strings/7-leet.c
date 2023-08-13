0cffc1427330240d1021829c2700e5bae44a597a
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-13 20:15:50 +0100

    7-leet.c2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,30 +10,32 @@
 char *leet(char *s)
 {
 	int index;
+	char leet_key[5] = "AEOTL";
+	char leet_value[5] = "43071";
 
 	index = 0;
 
 	while (*(s + index) != '\0')
 	{
-		if (*(s + index) == 'A' || *(s + index) == 'a')
+		int i;
+
+		/**
+		 * loop through all possible letters
+		 * that can be replaced.
+		 */
+		for (i = 0; i < 5; i++)
 		{
-			*(s + index) = '4';
-		}
-		else if (*(s + index) == 'E' || *(s + index) == 'e')
-		{
-			*(s + index) = '3';
-		}
-		else if (*(s + index) == 'O' || *(s + index) == 'o')
-		{
-			*(s + index) = '0';
-		}
-		else if (*(s + index) == 'T' || *(s + index) == 't')
-		{
-			*(s + index) = '7';
-		}
-		else if (*(s + index) == 'L' || *(s + index) == 'l')
-		{
-			*(s + index) = '1';
+			/**
+			 * if a letter in the string matches
+			 * a letter that can be replaced
+			 */
+			if (
+			(*(s + index) == leet_key[i])
+			|| (*(s + index) == (leet_key[i] + 32))
+			)
+			{
+				*(s + index) = leet_value[i];
+			}
 		}
 
 		index++;
