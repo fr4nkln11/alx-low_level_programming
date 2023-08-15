6fa17a210ae6a181f217188e0ebc89f4e9ef62cf
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-15 21:47:25 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - simple structure for dogs
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: A simple structure to define Dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
