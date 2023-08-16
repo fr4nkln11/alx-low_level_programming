6de42d1e6a84d2634375939da9e6d917696c2056
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-17 00:02:21 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: an array of argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
