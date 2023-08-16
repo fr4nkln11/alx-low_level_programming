f0becaf6f8cfe5a0c94d9fb14fb171bbda760341
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-17 00:07:35 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments
+ * @argv: an array of argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
