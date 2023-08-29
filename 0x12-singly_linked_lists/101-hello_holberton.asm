fb0eea7d9ebf51f353c4f5aeeb22f92e064b2474
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-29 12:23:04 +0100

    101-hello_holberton.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+global main
+extern printf
+
+section .text
+	main:
+		sub rsp, 8
+		mov rdi, string
+		call printf
+		add rsp, 8
+		ret
+
+section .data
+	string:
+		db "Hello, Holbertom", 0x0a, 0x00
