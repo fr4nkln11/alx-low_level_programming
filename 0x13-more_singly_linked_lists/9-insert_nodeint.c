558923ba21c99a0a22b4af8fc06b47c7f4b44364
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-29 14:14:49 +0100

    9-insert_nodeint.3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -54,10 +54,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 			return (new_node);
 		}
-		else
-		{
-			insert_nodeint_at_index(head, 0, n);
-		}
 	}
 	return (NULL);
 }
