aabc36bb2ecc71cb817db5836722626766b58d3c
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-30 11:02:02 +0100

    9-insert_nodeint.c5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *post_node;
 	listint_t *pre_node;
+
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 
 	if (!new_node)
