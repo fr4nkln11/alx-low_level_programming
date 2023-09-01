25f354820f19808aeb80860e1cb427ca6334babb
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-09-01 14:09:08 +0100

    5-flip_bits.c2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -19,8 +19,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	while (xor_mask)
 	{
-		bit_count += xor_mask & 1;
-		xor_mask = xor_mask >> 1;
+		/**
+		 * bit_count += xor_mask & 1;
+		 * xor_mask = xor_mask >> 1;
+		 */
+
+		bit_count++;
+		xor_mask = xor_mask & (xor_mask - 1);
 	}
 
 	return (bit_count);
