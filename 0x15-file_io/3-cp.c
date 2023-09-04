236196a629fbddf83ab15d429c0acdafe5d7aa51
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-09-05 00:41:46 +0100

    [Task 3] betty docs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,11 +4,20 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+void close_files(int source_fd, int dest_fd);
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 	int source_file_descriptor, dest_file_descriptor,
-		read_size, write_size,
-		sfd, dfd;
+		read_size, write_size;
 	char *buffer;
 	char *source_filename = argv[1];
 	char *dest_filename = argv[2];
@@ -21,8 +30,9 @@ int main(int argc, char *argv[])
 
 	buffer = (char *)malloc(1024);
 	source_file_descriptor = open(source_filename, O_RDONLY);
-	dest_file_descriptor = open(dest_filename, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	
+	dest_file_descriptor = open(dest_filename,
+			O_WRONLY | O_TRUNC | O_CREAT, 0664);
+
 	if (source_file_descriptor < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s", source_filename);
@@ -40,27 +50,43 @@ int main(int argc, char *argv[])
 		write_size = write(dest_file_descriptor, buffer, read_size);
 		if (write_size < 0)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", dest_filename);
 			exit(99);
 		}
 		free(buffer);
 		buffer = (char *)malloc(1024);
 	}
 	free(buffer);
-
-	sfd = close(source_file_descriptor);
-	dfd = close(dest_file_descriptor);
-
-	if (sfd < 0)
+	if (read_size < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", source_file_descriptor);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", source_filename);
+		exit(98);
 	}
 
-	if (dfd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", dest_file_descriptor);
-		exit(100);
-	}
+	close_files(source_file_descriptor, dest_file_descriptor);
 
 	return (0);
+}
+
+/**
+ * close_files - a function to close both files
+ * @source_fd: source_file_descriptor
+ * @dest_fd: dest_file_descriptor
+ *
+ * Return: nothing
+ */
+
+void close_files(int source_fd, int dest_fd)
+{
+	if (close(source_fd) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", source_fd);
+		exit(100);
+	}
+
+	if (close(dest_fd) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", dest_fd);
+		exit(100);
+	}
 }
