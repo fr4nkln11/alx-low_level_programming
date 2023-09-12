0f096caea6ddcb2c0e25f550c6612e4bbe95a4af
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-09-12 17:49:25 +0100

    3-alloc_grid.c5

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 			grid[y_pos] = malloc(sizeof(int) * width);
 			if (!grid[y_pos])
 			{
-				for (; (y_pos - 1) >= 0; y_pos--)
+				for (; y_pos >= 0; y_pos--)
 				{
 					free(grid[y_pos]);
 				}
