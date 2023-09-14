cb222e64555225e997cad3bdd250f8e75ad39f87
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-09-14 18:58:52 +0100

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,100 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int get_token_count(char *s);
+int get_token_length(char *s);
+
+char **strtow(char *str)
+{
+	if (str)
+	{
+		int token_count = get_token_count(str);
+		char **tokens = NULL;
+		char *current_token = NULL;
+		int token_index = 0;
+		int token_length;
+		int token_pos;
+
+		tokens = malloc(sizeof(char *) * (token_count + 1));
+		if (tokens)
+		{
+			for (; token_index < token_count; token_index++)
+			{
+				token_length = get_token_length(str);
+				tokens[token_index] = (char *)malloc(sizeof(char) * token_length);
+				if (!tokens[token_index])
+				{
+					return (NULL);
+				}
+				current_token = tokens[token_index];
+				token_pos = 0;
+
+				while (*str != '\0')
+				{
+					if (*str != ' ')
+					{
+						current_token[token_pos] = *str;
+						token_pos++;
+
+						if (*(str + 1) == ' ' || *(str + 1) == '\0')
+						{
+							current_token[token_pos] = '\0';
+							str++;
+							break;
+						}
+					}
+					str++;
+				}
+			}
+			tokens[token_index] = NULL;
+
+			return (tokens);
+		}
+	}
+
+	return (NULL);
+}
+
+int get_token_count(char *s)
+{
+	int i = 0;
+	int token_count = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) != ' ')
+				&& ((*(s + (i + 1)) == ' ') || (*(s + (i + 1)) == '\0')))
+		{
+			token_count++;
+		}
+		i++;
+	}
+
+	return (token_count);
+}
+
+int get_token_length(char *s)
+{
+	int i, token_length;
+
+	token_length = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) != ' ')
+		{
+			token_length++;
+
+			if (*(s + (i + 1)) == ' ' || *(s + (i + 1)) == '\0')
+			{
+				token_length++;
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return (token_length);
+}
