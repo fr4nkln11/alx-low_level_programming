aee39565f144aec328cb55e7c575d7d48890536e
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-14 01:15:14 +0100

    0x17 C [Task 8] 3

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - a function that deletes the node
+ * delete_dnodeint_at_index - a function that deletes the node
  * at index index of a dlistint_t linked list.
  * @head: pointer to head node
  * @index: position of node to delete
@@ -19,38 +19,38 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp;
 	unsigned int pos = 0;
 
-	if (head && *head)
+	if (!head && !(*head))
+		return (-1);
+	if (index == 0)
 	{
-		if (index <= 0)
-		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
-			if (*head)
-				(*head)->prev = NULL;
-		}
-		else
-		{
-			while (pos < index)
-			{
-				if (!cursor)
-					break;
-				cursor = cursor->next;
-				pos++;
-			}
-
-			if (pos < index || cursor == NULL)
-				return (-1);
-
-			post = cursor->next;
-			prev = cursor->prev;
-			free(cursor);
-			prev->next = post;
-			if (post)
-				post->prev = prev;
-
-			return (1);
-		}
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+		if (*head)
+			(*head)->prev = NULL;
+		return (1);
 	}
-	return (-1);
+	else
+	{
+		while (pos < index)
+		{
+			if (!cursor)
+				break;
+			cursor = cursor->next;
+			pos++;
+		}
+
+		if (pos < index || cursor == NULL)
+			return (-1);
+
+		post = cursor->next;
+		prev = cursor->prev;
+		free(cursor);
+		if (prev)
+			prev->next = post;
+		if (post)
+			post->prev = prev;
+
+		return (1);
+	}
 }
