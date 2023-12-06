df0e2b2fe8f8e9b98c135ee321f460badb6ef379
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-12-06 13:10:39 +0100

    implement collision handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,16 +13,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int size = ht->size;
 	unsigned long int index = key_index((unsigned char *)key, size);
 	hash_node_t *new_node = NULL;
+	hash_node_t *temp_node = NULL;
 
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
 		return (0);
 
+	/* create the new node */
 	new_node->key = (char *)key;
 	new_node->value = (char *)value;
 	new_node->next = NULL;
 
-	ht->array[index] = new_node;
+	/* check if index is already occupied (collision) */
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		temp_node = ht->array[index];
+		new_node->next = temp_node;
+		ht->array[index] = new_node;
+	}
 	return (1);
 }
--- 0x1A-hash_tables/custom_utils.c
+++ 0x1A-hash_tables/custom_utils.c
@@ -15,10 +15,14 @@ void print_hash_table(hash_table_t *table)
 
 		if (temp_node)
 		{
-			key = temp_node->key;
-			value = temp_node->value;
+			while (temp_node)
+			{
+				key = temp_node->key;
+				value = temp_node->value;
 
-			printf("[%lu]:[%s] ===> [%s]", i, key, value);
+				printf("[%lu]:[%s] ===> [%s]", i, key, value);
+				temp_node = temp_node->next;
+			}
 		}
 		else
 		{
