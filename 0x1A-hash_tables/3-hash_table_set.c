e4833fbcbfce567b281403e4781d21c3d4526812
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-12-06 14:39:12 +0100

    handle same keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,43 @@
 #include "hash_tables.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * create_hash_node - create a new hash node and return it
+ * @key: the key
+ * @value: the value
+ *
+ * Return: a pointer to the new node
+ */
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	new_node->key = (char *)malloc(strlen(key) + 1);
+	new_node->value = (char *)malloc(strlen(value) + 1);
+
+	if (!new_node)
+		return (NULL);
+
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
+
+	return (new_node);
+}
 
 /**
  * hash_table_set - a function that adds an element to the hash table
@@ -11,26 +49,23 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int size = ht->size;
 	unsigned long int index;
 	hash_node_t *new_node = NULL;
 	hash_node_t *temp_node = NULL;
 
+	if (ht == NULL)
+		return (0);
+
 	if (key[0] == '\0')
 		return (0);
 
-	index = key_index((unsigned char *)key, size);
-	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	index = key_index((unsigned char *)key, ht->size);
+	new_node = create_hash_node(key, value);
 
 	if (new_node == NULL)
 		return (0);
 
-	/* create the new node */
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
-	new_node->next = NULL;
-
-	/* check if index is already occupied (collision) */
+	/* check if index is not occupied */
 
 	if (ht->array[index] == NULL)
 	{
@@ -38,9 +73,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		temp_node = ht->array[index];
-		new_node->next = temp_node;
-		ht->array[index] = new_node;
+		/* update */
+		if (strcmp(ht->array[index]->key, new_node->key) == 0)
+		{
+			strcpy(ht->array[index]->value, value);
+		}
+		else
+		{
+			/* collision */
+			temp_node = ht->array[index];
+			new_node->next = temp_node;
+			ht->array[index] = new_node;
+		}
 	}
 	return (1);
 }
